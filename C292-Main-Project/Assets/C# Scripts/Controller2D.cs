8a65bbf64a718ca8577228cabedf0e0c07e79d19
KihrunS <kilosc24@gmail.com>
2025-10-23 15:14:34 -0400

    Added vertical collisions

--- C292-Main-Project/Assets/C# Scripts/Controller2D.cs
+++ C292-Main-Project/Assets/C# Scripts/Controller2D.cs
@@ -7,6 +7,8 @@ using UnityEngine;
 public class Controller2D : MonoBehaviour
 {
 
+    public LayerMask collisionMask;
+
     const float skinWidth = .015f;
     public int horizontalRayCount = 4;
     public int verticalRayCount = 4;
@@ -21,6 +23,37 @@ public class Controller2D : MonoBehaviour
     void Start()
     {
         collider = GetComponent<BoxCollider2D>();
+        CalculateRaySpacing();
+    }
+
+    public void Move(Vector3 velocity)
+    {
+        UpdateRaycastOrigins();
+
+        VerticalCollisions(ref velocity);
+
+        transform.Translate(velocity);
+    }
+
+    void VerticalCollisions(ref Vector3 velocity)
+    {
+        float directionY = Mathf.Sign(velocity.y);
+        float rayLength = Mathf.Abs(velocity.y) + skinWidth;
+
+        for (int i = 0; i < verticalRayCount; i++)
+        {
+            Vector2 rayOrigin = (directionY == -1) ? raycastOrigins.bottomLeft : raycastOrigins.topLeft;
+            rayOrigin += Vector2.right * (verticalRaySpacing * i + velocity.x);
+            RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.up * directionY, rayLength, collisionMask);
+
+            Debug.DrawRay(raycastOrigins.bottomLeft + Vector2.right * verticalRaySpacing * i, Vector2.up * -2, Color.red);
+
+            if (hit)
+            {
+                velocity.y = (hit.distance - skinWidth) * directionY;
+                rayLength = hit.distance;
+            }
+        }
     }
 
     void UpdateRaycastOrigins()
@@ -50,17 +83,5 @@ public class Controller2D : MonoBehaviour
     {
         public Vector2 topLeft, topRight;
         public Vector2 bottomLeft, bottomRight;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        UpdateRaycastOrigins();
-        CalculateRaySpacing();
-
-        for (int i = 0; i < verticalRayCount; i++)
-        {
-            Debug.DrawRay(raycastOrigins.bottomLeft + Vector2.right * verticalRaySpacing * i, Vector2.up * -2, Color.red);
-        }
     }
 }
--- C292-Main-Project/Assets/C# Scripts/Player.cs
+++ C292-Main-Project/Assets/C# Scripts/Player.cs
@@ -6,11 +6,24 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
+    float gravity = -20;
+    Vector3 velocity;
+
     Controller2D controller;
 
     // Start is called before the first frame update
     void Start()
     {
         controller = GetComponent<Controller2D>();
+    }
+
+    private void Update()
+    {
+        velocity.y += gravity * Time.deltaTime;
+    }
+
+    private void FixedUpdate() // Found this fix in a youtube comment under episode 2!: https://www.youtube.com/watch?v=OBtaLCmJexk&lc=UgxoMGzPq1m3koeDzZ94AaABAg
+    {
+        controller.Move(velocity * Time.deltaTime);
     }
 }
