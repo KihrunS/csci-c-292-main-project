e38dc1110a29e225ea1345b11f395d227fbff5ab
KihrunS <kilosc24@gmail.com>
2025-11-13 17:25:21 -0500

    Added room 4 and disappearing blocks

---
+++ C292-Main-Project/Assets/DisappearingBlock.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(SpriteRenderer))]
+
+public class DisappearingBlock : MonoBehaviour
+{
+    new BoxCollider2D collider;
+    SpriteRenderer spriteRenderer;
+
+    [SerializeField] float disappearTime;
+    [SerializeField] float respawnTime;
+
+
+    bool disappearing;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        collider = transform.parent.GetComponent<BoxCollider2D>();
+        spriteRenderer = transform.parent.GetComponent<SpriteRenderer>();
+        disappearing = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            if (!disappearing)
+            {
+                disappearing = true;
+                StartCoroutine("Disappear");
+            }
+        }
+    }
+
+    IEnumerator Disappear()
+    {
+        yield return new WaitForSeconds(disappearTime);
+        spriteRenderer.enabled = collider.enabled = false;
+        yield return new WaitForSeconds (respawnTime);
+        spriteRenderer.enabled = collider.enabled = true;
+        disappearing = false;
+
+    }
+}
